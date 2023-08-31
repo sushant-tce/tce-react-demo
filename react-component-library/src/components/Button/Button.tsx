45b7002ffdde9e71e2145b5a86c8a2852af7cedf
sushant-tce <sushant@technicalcareer.education>
2023-08-31 15:32:18 +0530

    setup

---
+++ react-component-library/src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+// import React, { FC, ButtonHTMLAttributes } from "react";
+// import ButtonMaterialUI from "@material-ui/core/Button";
+
+// interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+//   // Add any custom props you want to expose
+// }
+
+// const Button: FC<ButtonProps> = ({ children, ...props }) => {
+//   return <ButtonMaterialUI {...props}>{children}</ButtonMaterialUI>;
+// };
+
+// export default Button;
